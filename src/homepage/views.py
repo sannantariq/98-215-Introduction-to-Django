3b998ed0db9afc3a02afda3a4b1c388747637f91
Sannan Tariq <stariq@andrew.cmu.edu>
2016-02-08 18:14:14 -0500

    added connection from model to view

--- src/homepage/views.py
+++ src/homepage/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from homepage.models import blog
 
 # Create your views here.
 def home(request):
+	all_objects = blog.objects.all();
 	template = "home.html"
-	context = {}
+	context = {
+		"variable1" : all_objects,
+		"variable2" : all_objects[0],
+		"variable3" : all_objects[0].title,
+	}
 	return render(request, template, context)
+
