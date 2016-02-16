ecfa9e571fd6a68865f0c8fb7540cd9eb4509937
Sannan Tariq <stariq@andrew.cmu.edu>
2016-02-16 18:27:44 -0500

    pushed hw2

--- src/homepage/views.py
+++ src/homepage/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from homepage.models import blog
+from .forms import BlogPosts
 
 # Create your views here.
 def home(request):
 	all_objects = blog.objects.all();
 	template = "home.html"
+	form = BlogPosts(request.POST or None)
+	if form.is_valid():
+		variable = form.save(commit = 'false')
+		variable.save()
 	context = {
+		"form" : form,
 		"variable1" : all_objects,
 		"variable2" : all_objects[0],
 		"variable3" : all_objects[0].title,
 	}
+
 	return render(request, template, context)
 
